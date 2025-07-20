ba9f4668d649738b89f1faf24894ced1b6ca50ba
Zikou Liu <liuzikou@gmail.com>
2025-07-20 19:36:18 +1000

    Simplify vitest config: remove coverage to fix CI test issues

--- vitest.config.ts
+++ vitest.config.ts
@@ -6,13 +6,5 @@ export default defineConfig({
     globals: true,
     environment: 'jsdom',
     setupFiles: ['./src/test-setup.ts'],
-    coverage: {
-      provider: 'c8',
-      reporter: ['text', 'json', 'html'],
-      exclude: [
-        'node_modules/',
-        'src/test-setup.ts',
-      ],
-    },
   },
 }) 
